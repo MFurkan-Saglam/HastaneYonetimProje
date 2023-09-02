ea91d7180ccb26e1ca6863e3fcb2901924c631c1
MFurkan-Saglam <81245955+MFurkan-Saglam@users.noreply.github.com>
2023-09-03 02:35:02 +0300

    Update SqlBaglanti.cs

--- Hastane_Yonetim_Proje/SqlBaglanti.cs
+++ Hastane_Yonetim_Proje/SqlBaglanti.cs
@@ -7,14 +7,13 @@ using System.Data.SqlClient;
 namespace Hastane_Yonetim_Proje
 {
 
-    //bu sınıfı oluşturduk çünkü her formun arkasında veri tabanı var ve ben sürekli aynı şeyi yapmak istemiyorum...
 
     internal class SqlBaglanti
     {
-        //bir metot tanımlayayım ve her çağırdığımda bana sql bağlantısı yapsın bence mantıklı
+        //bir metot tanımlayayım ve her çağırdığımda bana sql bağlantısı versin
         public SqlConnection baglanti()
         {
-            SqlConnection conn = new SqlConnection("Data Source=DESKTOP-FELGMAP\\SQLEXPRESS;Initial Catalog=HastaneYonetimSistemi;Integrated Security=True");
+            SqlConnection conn = new SqlConnection("Data Source=--------------\\SQLEXPRESS;Initial Catalog=HastaneYonetimSistemi;Integrated Security=True");
             conn.Open();
             return conn;
         
